6e02146223cf46741cfe47c69f6b615bc8226da7
Bruno Antunes <bmvantunes@gmail.com>
2020-10-24 22:39:07 +0100

    setTimeout examples

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export interface CounterProps {
   description: string;
@@ -7,11 +7,35 @@ export interface CounterProps {
 export function Counter({ description }: CounterProps) {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    const id = setTimeout(() => {
+      console.log('from cancel timeout', description);
+    }, 3000);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [description]);
+
+  useEffect(() => {
+    let currentRender = true;
+
+    setTimeout(() => {
+      if (currentRender) {
+        console.log('from variable', description);
+      } else {
+        console.log('UPS I am done');
+      }
+    }, 3000);
+
+    return () => {
+      currentRender = false;
+    };
+  }, [description]);
+
   return (
     <div>
-      <h5>
-        DESC: {description}
-      </h5>
+      <h5>DESC: {description}</h5>
 
       <button onClick={() => setCount(count - 1)}>-</button>
       {count}
